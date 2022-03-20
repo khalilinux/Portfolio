648af147bd4f70131647082c6dea4f343e0f4d11
khalil_b <K.BELGUITH@openbee.com>
2022-03-20 23:33:01 +0100

    afficher les donnée owner

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,4 +1,6 @@
+using Core.interfaces;
 using Infrastructure;
+using Infrastructure.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +34,7 @@ namespace Web
             {
                 options.UseSqlServer(configuration.GetConnectionString("MyPortfolioDB"));
             });
+            services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
